39c72fcaad411a85474bf7390e2241b46c288042
npiriou <71148909+npiriou@users.noreply.github.com>
2020-09-13 22:14:20 +0200

    Tout marche !
    Lumber et vente ajoutés, toutes les vagues et cartes sans passif sont ajoutées. 
    
    Il manque les cartes avec passif et les passifs des vagues.

--- scripts1.js
+++ scripts1.js
@@ -7,19 +7,32 @@ function vague(numero, nom, resiTr, resiPe, resiMa, precision, nombre) {
 	this.precision = precision;
 	this.nombre = nombre;
 }
-var vague0 = new vague(0, "Crabes", 2, 3, 2, 5, 5);
-var vague1 = new vague(1, "Loups", 3, 3, 2, 5, 6);
-var vague2 = new vague(2, "Serpents vicieux", 5, 4, 3, 3, 4);
-var vague3 = new vague(3, "Trolls", 4, 4, 5, 4, 8);
-var vague4 = new vague(4, "Aigles", 6, 2, 5, 4, 7);
 
-var tabVagues = [vague0, vague1, vague2, vague3, vague4];
+var tabVagues = [];
+tabVagues[0] = new vague(1, "Crabes", 2, 3, 2, 5, 5);
+tabVagues[1] = new vague(2, "Loups", 3, 3, 2, 5, 6);
+tabVagues[2] = new vague(3, "Serpents vicieux", 5, 4, 3, 3, 4);
+tabVagues[3] = new vague(4, "Trolls", 4, 4, 5, 4, 8);
+tabVagues[4] = new vague(5, "Aigles", 6, 2, 5, 4, 7);
+tabVagues[5] = new vague(6, "Assassins", 5, 6, 4, 2, 6);
+tabVagues[6] = new vague(7, "Raptors", 3, 3, 3, 3, 9);
+tabVagues[7] = new vague(8, "Empoisonneurs", 4, 5, 999, 3, 6);
+tabVagues[8] = new vague(9, "Golems", 5, 6, 3, 4, 12);
+tabVagues[9] = new vague(10, "Boss", 999, 6, 6, 2, 6);
+tabVagues[10] = new vague(11, "Centaures", 3, 5, 4, 4, 15);
+tabVagues[11] = new vague(12, "Fanatiques", 3, 5, 5, 3, 12);
+tabVagues[12] = new vague(13, "Voleurs", 3, 5, 4, 4, 15);
+tabVagues[13] = new vague(14, "Ptérodactyles", 6, 2, 4, 2, 10);
+tabVagues[14] = new vague(15, "Ethérés", 999, 999, 4, 2, 10);
+tabVagues[15] = new vague(16, "Mammouths", 3, 5, 4, 4, 15);
+
+var lumber = 0;
 var v = 0;
 var vagueActuelle = tabVagues[v];
 
 function displayVagueActuelle() {
 	status2 = document.getElementById("status2");
-	status2.innerHTML = ("Vague " + ((vagueActuelle.numero) + 1) + " : " + vagueActuelle.nom + "<br>"
+	status2.innerHTML = ("Vague " + ((vagueActuelle.numero)) + " : " + vagueActuelle.nom + "<br>"
 		+ (vagueActuelle.nombre) + " ennemis au départ.<br>"
 		+ "Résistances au attaques Tranchantes : " + vagueActuelle.resiTr + " ou plus.<br>"
 		+ "Résistances au attaques Perçantes : " + vagueActuelle.resiPe + " ou plus.<br>"
@@ -31,6 +44,9 @@ displayVagueActuelle();
 
 
 //initialisation
+var status = document.getElementById("status");
+let newP = document.createElement('p');
+
 var nbMobsReste = vagueActuelle.nombre;
 var nbMobsTourPrec = vagueActuelle.nombre;
 var gold = 20;
@@ -41,6 +57,8 @@ var degatsRestants = null;
 $(':input').prop('disabled', true);
 var boutonCombat = document.getElementById("boutonCombat");
 boutonCombat.disabled = false;
+var boutonAchatLumber = document.getElementById("boutonAchatLumber");
+boutonAchatLumber.disabled = false;
 
 // desactive le bouton pour roll les mobs
 var tour = "joueur";
@@ -198,7 +216,7 @@ function rollMob() {
 
 	var status = document.getElementById("status");
 	let newP = document.createElement('p');
-	status.append(newP, "Les ennemis attaquent ! Vous perdez " + degatsInfliges + " PV ! ");
+	status.append(newP, "Les "+vagueActuelle.nom+" attaquent ! Vous perdez " + degatsInfliges + " PV ! ");
 
 	degatsRestants = degatsInfliges;
 	repartitionDegats();
@@ -226,18 +244,30 @@ function rollMob() {
 // }
 // }
 function repartitionDegats() {
-	 if(boardVide(board)){
+
+	// si le board est vide, ça leak et la vague se termine
+	if (boardVide(board)) {
 		degatsRestants = 0;
-	tour = "boutique";
+		tour = "boutique";
 		var boutonRoll = document.getElementById("boutonRoll");
 		boutonRoll.disabled = true;
-		boutonRollMob.disabled = true;		
-		boutique();}
-	//tant qu'il reste des pv a repartir ET qu'il reste au moins une tour en front line
-	
+		boutonRollMob.disabled = true;
+		boutique();
+	}
+
+
+	//tant qu'il reste des pv a repartir ET qu'il reste au moins une tour en front line	
 	if ((degatsRestants > 0) && (board[0] != 0 || board[1] != 0 || board[2] != 0 || board[3] != 0)) {
 
-
+		// les dégats partent en premier sur les cartes blessées
+		for (i = 0; i < 4; i++) {
+			if (board[i].pvact < board[i].pvdep) {
+				carteBlessee = i;
+				selectionPVPerdu(i);
+			}
+		}
+		//si apres la repartition auto il reste des pv a repartir ET qu'il reste au moins une tour en front line	
+		if ((degatsRestants > 0) && (board[0] != 0 || board[1] != 0 || board[2] != 0 || board[3] != 0)) {
 		var status = document.getElementById("status");
 		let newP = document.createElement('p');
 		status.append(newP, "Il reste " + (degatsRestants) + " PV perdus à répartir. Sélectionnez une carte en première ligne. ");
@@ -247,13 +277,21 @@ function repartitionDegats() {
 			$("#Cell" + (i + 1))[0].onclick = (function (temp2) { return function () { selectionPVPerdu(temp2) }; })(i);
 		}
 	}
-	else {
-		var tour = "joueur";
-		var boutonRoll = document.getElementById("boutonRoll");
-		boutonRoll.disabled = false;
-		return;
 	}
-	
+	else {
+		// si la frontline a une place et que il ya des cartes en backline, elles doivent avancer
+		if ((board[0] == 0 || board[1] == 0 || board[2] == 0 || board[3] == 0)
+			&& (board[4] != 0 || board[5] != 0 || board[6] != 0 || board[7] != 0)) {
+			backlineAvance();
+		}
+		else {
+			donnerBonsDes(); // on réactualise le  nombre de dés
+			var tour = "joueur";
+			var boutonRoll = document.getElementById("boutonRoll");
+			boutonRoll.disabled = false;
+			return;
+		}
+	}
 }
 
 
@@ -264,10 +302,10 @@ function selectionPVPerdu(posCarte) {
 
 	if (board[posCarte].pvact <= degatsRestants) { // plus de degats que de pv, la carte meurt
 		status.append(newP, board[posCarte].nom + " est mort");
-			 
-		 var Celli = $("#Cell" + (posCarte + 1))[0];	
-		 var carteMi = board[posCarte];
-		 Celli.innerHTML = "Place Vide";
+
+		var Celli = $("#Cell" + (posCarte + 1))[0];
+		var carteMi = board[posCarte];
+		Celli.innerHTML = "Place Vide";
 		// conclusion
 		degatsRestants = degatsRestants - board[posCarte].pvact;
 		board[posCarte] = 0;
@@ -276,9 +314,9 @@ function selectionPVPerdu(posCarte) {
 	else if (board[posCarte].pvact > degatsRestants) { // plus de pv que de dégats, la carte tanke
 		board[posCarte].pvact = board[posCarte].pvact - degatsRestants;
 		status.append(newP, board[posCarte].nom + " survit avec " + board[posCarte].pvact + " PV !");
-		 var Celli = $("#Cell" + (posCarte + 1))[0];
-		 var carteMi = board[posCarte];
-		 Celli.innerHTML = template.format(carteMi.tier, carteMi.nom, carteMi.nbAttTr, carteMi.nbAttPe, carteMi.nbAttMa, carteMi.pvact, carteMi.pvdep);
+		var Celli = $("#Cell" + (posCarte + 1))[0];
+		var carteMi = board[posCarte];
+		Celli.innerHTML = template.format(carteMi.tier, carteMi.nom, carteMi.nbAttTr, carteMi.nbAttPe, carteMi.nbAttMa, carteMi.pvact, carteMi.pvdep);
 		degatsRestants = 0;
 		repartitionDegats();
 	}
@@ -294,7 +332,7 @@ function reponseDesMonstres() {
 		tabDicesMob[0].remove();
 	}
 
-	
+
 
 	nbMobsTourPrec = nbMobsReste;
 	nbDesMobs = tabDicesMob.length;
@@ -315,9 +353,32 @@ function reponseDesMonstres() {
 
 
 // return vrai si le board est vide, faux sinon
-function boardVide(board){
+function boardVide(board) {
 	for (i = 0; i < 8; i++) {
-		if((board[i]!= 0)) return false;
+		if ((board[i] != 0)) return false;
 	}
-  return true;
+	return true;
+}
+
+function backlineAvance() {
+	var status = document.getElementById("status");
+	let newP = document.createElement('p');
+	status.append(newP, "Faites avancer votre backline");
+
+	// on réactive le clic sur les cartes du board pour les select
+	for (i = 0; i < 8; i++) {
+		$("#Cell" + (i + 1))[0].onclick = (function (temp2) { return function () { selectionCarteBack(temp2) }; })(i);
+	}
+
+}
+function selectionCarteBack(posNouvelleCarteSelect) {
+	if ((posCarteSelect == null) && (posNouvelleCarteSelect > 3) && board[posNouvelleCarteSelect] != 0) {
+		posCarteSelect = posNouvelleCarteSelect;
+		$("#Cell" + (posCarteSelect + 1))[0].style.background = "blue";
+	}
+	else if ((posCarteSelect != null) && (posNouvelleCarteSelect < 4) && board[posNouvelleCarteSelect] == 0) {
+		deplacerCarteBoard(board, posCarteSelect, posNouvelleCarteSelect)
+		$("#Cell" + (posCarteSelect + 1))[0].style.background = "white"; posCarteSelect = null;
+		repartitionDegats();
+	}
 }
--- scripts2.js
+++ scripts2.js
@@ -11,17 +11,29 @@ function carte(numero, tier, nom, pvdep, pvact, nbAttTr, nbAttPe, nbAttMa, passi
     this.nbAttMa = nbAttMa;
     this.passif = passif;
 }
+var deck1 = [];
+deck1[0] = new carte(1, 1, "Spartiate", 1, 1, 1, 1, 0, null);
+deck1[1] = new carte(20, 5, "Yéti", 5, 5, 1, 1, 1, null);
+deck1[2] = new carte(2, 1, "Grosse Mite", 1, 1, 0, 2, 0, null);
+deck1[3] = new carte(3, 1, "Croisé", 1, 1, 1, 0, 1, null);
+deck1[4] = new carte(4, 1, "Péon", 2, 2, 0, 1, 0, null);
+deck1[5] = new carte(5, 1, "Barbare", 1, 1, 2, 0, 0, null);
+deck1[6] = new carte(6, 1, "Petite Tortue", 2, 2, 0, 0, 1, null);
+deck1[7] = new carte(7, 1, "Champignon", 2, 2, 0, 0, 1, null);
+deck1[8] = new carte(8, 1, "Nain", 2, 2, 1, 0, 0, null);
+deck1[9] = new carte(9, 1, "Magneto", 1, 1, 0, 1, 1, null);
+deck1[10] = new carte(10, 1, "Barricade", 3, 3, 0, 0, 0, null);
+deck1[11] = new carte(11, 2, "Bourrin", 2, 2, 1, 0, 1, null);
+deck1[12] = new carte(12, 2, "Ermite", 2, 2, 0, 1, 1, null);
+deck1[13] = new carte(13, 2, "Croketzor", 1, 1, 1, 1, 1, null);
+deck1[14] = new carte(14, 2, "Bébé Panda", 3, 3, 0, 1, 0, null);
+deck1[15] = new carte(15, 2, "Jacky", 1, 1, 1, 2, 0, null);
+deck1[16] = new carte(16, 2, "Mur", 4, 4, 0, 0, 0, null);
+deck1[17] = new carte(17, 4, "Sam", 1, 1, 2, 2, 1, null);
+deck1[18] = new carte(18, 4, "Chevalier", 2, 2, 2, 1, 1, null);
+deck1[19] = new carte(19, 4, "Panda", 6, 6, 0, 1, 0, null);
 
-var carte1t1 = new carte(1, 1, "Spartiate", 1, 1, 1, 1, 0, null);
-var carte2t1 = new carte(2, 1, "Grosse Mite", 1, 1, 0, 2, 0, null);
-var carte3t1 = new carte(3, 1, "Croisé", 1, 1, 1, 0, 1, null);
-var carte4t1 = new carte(4, 1, "Péon", 2, 2, 0, 1, 0, null);
-var carte5t1 = new carte(5, 1, "Barbare", 1, 1, 2, 0, 0, null);
-var carte6t1 = new carte(6, 1, "Petite Tortue", 2, 2, 0, 0, 1, null);
-var carte7t1 = new carte(7, 1, "Champignon", 2, 2, 0, 0, 1, null);
-var carte8t1 = new carte(8, 1, "Nain", 2, 2, 1, 0, 0, null);
 
-var deck1 = [carte1t1, carte2t1, carte3t1, carte4t1, carte5t1, carte6t1, carte7t1, carte8t1];
 
 var board = [0, 0, 0, 0, 0, 0, 0, 0];
 var boardPreCombat = board;  //servira a sauvegarder le board pour le récupérer après le combat, pour résu les morts etc
@@ -65,7 +77,7 @@ var template = `<div id="tier" class="bloc-Gauche">
 {2} Tranchantes<br>
 {3} Perçantes<br>
 {4} Magiques<br>
-Passif
+<!-- Le passif viendra ici-->
 </div>  </div>
 <div id="carteDivision">
 <div id="pvact" class="carteDivision">
@@ -101,10 +113,10 @@ function distribution() {
         // on réactive le clic sur les cartes de la main pour pouvoir les acheter
         $("#CellM" + (i + 1))[0].onclick = (function (temp) { return function () { pickM(temp) }; })(i);
     }
-// on réactive le clic sur les cartes du board pour les select
-for (i = 0; i <8; i++) {
-$("#Cell" + (i + 1))[0].onclick = (function (temp1, temp2) { return function () { selectionCarteBoard(temp1, temp2) }; })(board, i);
-}
+    // on réactive le clic sur les cartes du board pour les select
+    for (i = 0; i < 8; i++) {
+        $("#Cell" + (i + 1))[0].onclick = (function (temp1, temp2) { return function () { selectionCarteBoard(temp1, temp2) }; })(board, i);
+    }
 }
 
 distribution();
@@ -131,51 +143,70 @@ function pickM(n) { // place des cartes de la main sur le board quand on clic de
 
 }
 
-var posCarteSelect= null;
+var posCarteSelect = null;
 // selectionne les cartespour les déplacer
 
-function selectionCarteBoard(boardA, posNouvelleCarteSelect){
-if(posCarteSelect==null){posCarteSelect = posNouvelleCarteSelect;
-    $("#Cell"+(posCarteSelect+1))[0].style.background = "blue";
-}
-else if (posCarteSelect != null){deplacerCarteBoard(boardA, posCarteSelect, posNouvelleCarteSelect)
-     $("#Cell"+(posCarteSelect+1))[0].style.background = "white"; posCarteSelect= null;
-}
-}
+function selectionCarteBoard(boardA, posNouvelleCarteSelect) {
+    if (posCarteSelect == null) {
+        posCarteSelect = posNouvelleCarteSelect;
+        $("#Cell" + (posCarteSelect + 1))[0].style.background = "blue";
 
+        if(board[posCarteSelect] !=0) {$("#boutonVente")[0].disabled=false;}
+    }
+    else if (posCarteSelect != null) {
+        deplacerCarteBoard(boardA, posCarteSelect, posNouvelleCarteSelect)
+        $("#Cell" + (posCarteSelect + 1))[0].style.background = "white"; posCarteSelect = null;
+        $("#boutonVente")[0].disabled=true;
 
-function deplacerCarteBoard(boardA, posdep, posarr){
-temp = boardA[posarr];
-boardA[posarr]= boardA[posdep];
-boardA[posdep] = temp;
-
-var Celldep = $("#Cell" + (posdep + 1))[0];
-var Cellarr = $("#Cell" + (posarr + 1))[0];
-
-temp = Cellarr.innerHTML;
-Cellarr.innerHTML = Celldep.innerHTML;
-Celldep.innerHTML = temp;
+    }
 }
 
 
-function boutique() {  // à terminer, faut rajouter le placement, etc
+function deplacerCarteBoard(boardA, posdep, posarr) {
+    temp = boardA[posarr];
+    boardA[posarr] = boardA[posdep];
+    boardA[posdep] = temp;
 
-    gold = gold + vagueActuelle.nombre;
+    var Celldep = $("#Cell" + (posdep + 1))[0];
+    var Cellarr = $("#Cell" + (posarr + 1))[0];
+
+    temp = Cellarr.innerHTML;
+    Cellarr.innerHTML = Celldep.innerHTML;
+    Celldep.innerHTML = temp;
+}
+
+
+function boutique() {  // à terminer, 
+
+    // on re desactive le bouton roll pour etre sur
+    boutonRoll = document.getElementById("boutonRoll");
+    boutonRoll.disabled = true;
+
+    document.getElementById("boutonAchatLumber").disabled = false;
+
+    // calcul des golds 
+    tabDices = document.getElementsByClassName("mob");
+    goldGagnes = parseFloat(vagueActuelle.nombre) - parseFloat(tabDices.length);
+    if (goldGagnes < 0) { goldGagnes = 0; }
+    gold = parseFloat(gold) + parseFloat(goldGagnes)+parseFloat(lumber);
+
+
     var status = document.getElementById("status");
     let newP = document.createElement('p');
-    status.append(newP, "Vague terminée. Vous gagnez " + vagueActuelle.nombre + " gold. ");
+    status.append(newP, "Vague terminée. Vous gagnez " + goldGagnes + " gold des ennemis et "+lumber+" de vous ouvriers.");
 
     //le board revient comme avant le combat
-    board=copyBoard(boardPreCombat);
+    board = copyBoard(boardPreCombat);
     afficherBoard(board);
-    
+
     for (i = 0; i < 8; i++) {
-        if(board[i]!=0){
-        var Celli = $("#Cell" + (i + 1))[0];
-        var cartei = board[i];
-        board[i].pvact=board[i].pvdep;
-        Celli.innerHTML = template.format(cartei.tier, cartei.nom, cartei.nbAttTr, cartei.nbAttPe, cartei.nbAttMa, cartei.pvdep, cartei.pvdep);
-    }}
+        if (board[i] != 0) {
+            var Celli = $("#Cell" + (i + 1))[0];
+            var cartei = board[i];
+            board[i].pvact = board[i].pvdep;
+            Celli.innerHTML = template.format(cartei.tier, cartei.nom, cartei.nbAttTr, cartei.nbAttPe, cartei.nbAttMa, cartei.pvdep, cartei.pvdep);
+        }
+    }
     // distribution des nouvelles cartes et mise a jour des gold
     distribution();
     $("#sectiongold")[0].innerHTML = gold + " gold;"
@@ -189,25 +220,36 @@ function boutique() {  // à terminer, faut rajouter le placement, etc
 function combat() {
 
     // verfication du placement en frontline
-    if ((board[0]==0||board[1]==0||board[2]==0||board[3]==0)&&(board[4]!=0||board[5]!=0||board[6]!=0||board[7]!=0)){
+    if ((board[0] == 0 || board[1] == 0 || board[2] == 0 || board[3] == 0) && (board[4] != 0 || board[5] != 0 || board[6] != 0 || board[7] != 0)) {
         var status = document.getElementById("status"); // on vide la chat box
         let newP = document.createElement('p');
         status.append(newP, "Vous devez remplir la première ligne avant de pouvoir mettre des cartes en deuxième ligne.");
     }
 
-else{ // si le placement est autorisé
+    else { // si le placement est autorisé
 
-    boardPreCombat=copyBoard(board); // on save le board
+        boardPreCombat = copyBoard(board); // on save le board
 
-    $("#boutonCombat")[0].disabled = true; //desactivation du bouton Paré au combat
 
-    for(i=0;i<5;i++){ // désactivation du clic sur les cartes de la main pour les acheter
-    $("#CellM" + (i + 1))[0].onclick = null;
-    }
-    for(i=0;i<8;i++){ // désactivation du clic sur les cartes du board pour les selectionner et déplacer
-        $("#Cell" + (i + 1))[0].onclick = null;
+        document.getElementById("boutonAchatLumber").disabled = true;
+        $("#boutonCombat")[0].disabled = true; //desactivation du bouton Paré au combat
+
+        for (i = 0; i < 5; i++) { // désactivation du clic sur les cartes de la main pour les acheter
+            $("#CellM" + (i + 1))[0].onclick = null;
         }
+        for (i = 0; i < 8; i++) { // désactivation du clic sur les cartes du board pour les selectionner et déplacer
+            $("#Cell" + (i + 1))[0].onclick = null;
+        }
+        nbMobsReste = vagueActuelle.nombre;
+        donnerBonsDes(); // on donne le bon nombre de dés
+        boutonRoll.disabled = false; // on réactive le bouton pour lancer les dés
 
+        var status = document.getElementById("status"); // on vide la chat box
+        status.innerHTML = " ";
+    }
+}
+
+function donnerBonsDes() {
     // on compte le nombre d'attaques pour mettre autant de dés
     nbTotAttTr = 0;
     nbTotAttPe = 0;
@@ -222,38 +264,62 @@ else{ // si le placement est autorisé
     $(".dice").remove(); // on enleve les anciens dés
 
     // on met le bon nombre de dés
-    nbMobsReste = vagueActuelle.nombre;
-    for (i = 0; i < nbMobsReste; i++) {        addDiceMob();    }
-    for (i = 0; i < nbTotAttTr; i++) {        addDiceT();    }
-    for (i = 0; i < nbTotAttPe; i++) {        addDiceP();    }
-    for (i = 0; i < nbTotAttMa; i++) {        addDiceM();    }
-    boutonRoll.disabled = false; // on réactive le bouton pour lancer les dés
 
-    var status = document.getElementById("status"); // on vide la chat box
-    status.innerHTML = " ";
-}}
+    for (i = 0; i < nbMobsReste; i++) { addDiceMob(); }
+    for (i = 0; i < nbTotAttTr; i++) { addDiceT(); }
+    for (i = 0; i < nbTotAttPe; i++) { addDiceP(); }
+    for (i = 0; i < nbTotAttMa; i++) { addDiceM(); }
+
+}
 
 // Affiche Board
 function afficherBoard(board) {
-	// écrit les carac des cartes du board sur le board
-	for (i = 0; i < 8; i++) {
-		if (board[i] != 0) {
-			var CellMi = $("#Cell" + (i+1))[0];
-			var carteMi = board[i];
-			CellMi.innerHTML = template.format(carteMi.tier, carteMi.nom, carteMi.nbAttTr, carteMi.nbAttPe, carteMi.nbAttMa, carteMi.pvact, carteMi.pvdep);
-		}else{
-			CellMi.innerHTML = "Place Vide";
-		}
-
-    }}
-    
-// Utilisez cette fonction pour copier un board    
-    function copyBoard(board){
-        try{
-            var copy = JSON.parse(JSON.stringify(board));
-        } catch(ex){
-            alert("Sagarex pose une alerte");
+    // écrit les carac des cartes du board sur le board
+    for (i = 0; i < 8; i++) {
+        var CellMi = $("#Cell" + (i + 1))[0];
+        if (board[i] != 0) {
+            
+            var carteMi = board[i];
+            CellMi.innerHTML = template.format(carteMi.tier, carteMi.nom, carteMi.nbAttTr, carteMi.nbAttPe, carteMi.nbAttMa, carteMi.pvact, carteMi.pvdep);
+        } else {
+            CellMi.innerHTML = "Place Vide";
         }
-        return copy;
+
+    }
+}
+
+// Utilisez cette fonction pour copier un board    
+function copyBoard(board) {
+    try {
+        var copy = JSON.parse(JSON.stringify(board));
+    } catch (ex) {
+        alert("Sagarex pose une alerte");
+    }
+    return copy;
+}
+
+function achatLumber() {
+    if (gold >= 5) {
+        lumber++;
+        gold = gold - 5;
+        $("#sectiongold")[0].innerHTML = gold + " gold";
+        $("#sectionlumber")[0].innerHTML = lumber + "  ouvriers";
     }
 
+}
+
+function vente(){
+    if (posCarteSelect != null) {
+        
+        
+        $("#Cell" + (posCarteSelect + 1))[0].style.background = "white"; 
+        gold = parseFloat(parseFloat(gold)+parseFloat(board[posCarteSelect].tier));
+        $("#sectiongold")[0].innerHTML = gold + " gold";
+
+
+        board[posCarteSelect] =0;
+        posCarteSelect = null;
+        afficherBoard(board);
+        
+        
+}}
